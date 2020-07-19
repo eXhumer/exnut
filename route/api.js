ee8efbe4eaf2ecd97beeacd2164ae4499095e4e7
eXhumer <eXhumer@member.fsf.org>
2020-07-18 18:50:01 -0600

    Implement endpoints properly (user, search, scan)

--- route/api.js
+++ route/api.js
@@ -1,32 +1,38 @@
-const fs = require("fs");
+const fs = require("fs/promises");
+const { constants } = require("fs");
+const path = require("path");
 const express = require("express");
 const auth = require("./auth");
 const googleDriveService = require("../gdrive");
 const config = require("../config");
 
+const isWindows = process.platform === "win32";
+
 const router = express.Router();
 
+// only use auth if the server is configured to do so
 if(config.server.requireAuth)
     router.use(/^\/api\/(.+)/, auth);
 
 router
     .route("/api/user/")
     .get((req, res) => {
-        const auth = Buffer.from(req.headers.authorization.split(" ")[1], "base64").toString("utf-8").split(":");
-        res.json({
-            "id": auth[0],
-            "password": auth[1],
-            "isAdmin": false,
-            "remoteAddr": "3",
-            "requireAuth": config.server.requireAuth,
-            "switchHost": "2",
-            "switchPort": 1
-        });
+        const resObj = { requireAuth: config.server.requireAuth };
+        // if auth is required, add id/password along with requireAuth
+        if(resObj.requireAuth) {
+            const auth = Buffer.from(req.headers.authorization.split(" ")[1], "base64").toString("utf-8").split(":");
+            Object.assign(resObj, {id: auth[0], password: auth[1]});
+        }
+        res.json(resObj);
     });
 
 router
     .route("/api/search/")
-    .get((req, res) => {res.json([{"id": "TITLE_ID", "name": "FILE_NAME", "version": 0}]);});
+    .get(async (req, res) => {
+        const filesStr = await fs.readFile(__dirname + "/../conf/files.conf", "utf8");
+        const files = JSON.parse(filesStr);
+        res.json(files);
+    });
 
 router
     .route("/api/updateDb/")
@@ -34,19 +40,50 @@ router
 
 router
     .route("/api/scan/")
-    .get((req, res) => {res.send("OK!");});
+    .get(async (req, res) => {
+        let scannedTitles = [];
+        let scanPromises = [];
+        for (const scanStr of config.paths.scan) {
+            const scanPath = path.resolve(scanStr);
+            try {
+                await fs.access(scanPath, constants.R_OK);
+                scanPromises = scanPromises.concat(fs.readdir(scanPath));
+            } catch(err) {
+                console.log(`Unable to access folder ${scanPath}. Skipping...`);
+            }
+        }
+        if(scanPromises.length > 0) {
+            const scannedFilesArr = await Promise.all(scanPromises);
+            const scannedFiles = scannedFilesArr[0];
+            const nswRegex = /^(?:([\S]+)\s*)?\[([A-Fa-f\d]{16})\]\s*(?:\[([A-Z]{2})\]\s*)?\[v([\d]+)\](?:\s*\[CR-(\d{2})\])?.(nsp|nsz|xci|xcz)$/;
+            scannedTitles = await scannedFiles.reduce(async (res, filePromise) => {
+                const file = await filePromise;
+                const arr = await res;
+                const regexMatch = path.basename(file).match(nswRegex);
+                if(regexMatch) {
+                    arr.push({id: regexMatch[2], name: file, version: Number(regexMatch[4])})
+                }
+                return arr;
+            }, scannedTitles);
+            await fs.writeFile(__dirname + "/../conf/files.conf", JSON.stringify(scannedTitles), "utf8");
+        }
+        res.json({success: true, result: scannedTitles.length});
+    });
 
 router
     .route("/api/gdriveToken/")
     .get(async (req, res) => {
+        // try getting the gdrive service for server
         const gdriveService = await googleDriveService();
         if(gdriveService) {
+            // if it exists, read credentials/token & send them
             const credentials = JSON.parse(fs.readFileSync(__dirname + "/../conf/credentials.json"));
             const token = JSON.parse(fs.readFileSync(__dirname + "/../conf/gdrive.token"));
             res.json({"access_token": token.access_token, "refresh_token": token.refresh_token, "credentials": credentials});
-            return;
+        } else {
+            // send 401
+            res.status(401).setHeader().json({success: false, result: "Web server missing Google application credentials"});
         }
-        res.status(401).json({"success": false, "result": "Web server missing Google authorization credentials"})
     });
 
 router
